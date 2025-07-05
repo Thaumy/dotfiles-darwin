2f7bf001cdaab55abf83b0f4e192269df37bd25b
Thaumy Cheng <thaumy@outlook.com>
2025-07-05 20:41:12 +0800

    sync git config

---
+++ git/scripts/rl.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+git reflog \
+  --oneline \
+  --pretty='%C(yellow)🠵 %h %>(9)%C(green)%gd %C(cyan)%an %<|(80)%C(black)%gs %C(dim green)%s %C(auto)%(decorate:prefix=[,suffix=],pointer==,tag=🏷 )'
