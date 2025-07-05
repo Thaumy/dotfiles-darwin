cb2eb534364616c4aa61b07d496da16a5f9dbce0
Thaumy <thaumy@outlook.com>
2025-07-05 20:41:12 +0800

    sync git config

---
+++ git/scripts/s.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+git log \
+  --oneline \
+  --date format:'%y-%m-%d %H:%M' \
+  --pretty='%C(yellow)🠷 %h %C(green)%cd %C(cyan)%an %C(dim green)%s %C(auto)%(decorate:prefix=[,suffix=],pointer==,tag=🏷 )' \
+  --reverse \
+  -n3
+
+git status
---
+++ git/scripts/undo.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+if [ "$1" = 'c' ]; then
+  git reset HEAD^
+  exit
+fi
+
+if [ "$1" = 'm' ]; then
+  if [ -z "$2" ]; then
+    git restore .
+  else
+    git restore "$2"
+  fi
+  exit
+fi
+
+if [ "$1" = 'a' ]; then
+  if [ -z "$2" ]; then
+    git restore --staged .
+  else
+    git restore --staged "$2"
+  fi
+  exit
+fi
+
+if [ "$1" = 'n' ]; then
+  if [ -z "$2" ]; then
+    git clean -df
+  else
+    rr "$2"
+  fi
+  exit
+fi
+
+echo 'available options:'
+echo 'c: undo commit'
+echo 'm [path]: undo modify'
+echo 'a [path]: undo add'
+echo 'n [path]: undo new files'
