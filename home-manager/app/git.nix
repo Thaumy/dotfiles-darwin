87aaf677bb4734a421f3e19db18036e8ff24f3f6
Thaumy Cheng <thaumy@outlook.com>
2025-07-25 17:17:06 +0800

    sync HM git config

---
+++ home-manager/app/git.nix
@@ -0,0 +1,16 @@
+{ config, ... }:
+let
+  homeDir = config.home.homeDirectory;
+in
+{
+  programs.git = {
+    enable = true;
+
+    lfs.enable = true;
+    ignores = [ ".idea" ".vscode" ".thaumy" ".DS_Store" ];
+    includes = [
+      { path = "${homeDir}/cfg/git/config.ini"; }
+      { path = "${homeDir}/cfg/git/aliases.ini"; }
+    ];
+  };
+}
