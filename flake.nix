ef55c898be09e94742699d1a32a59148f6711e6a
Thaumy Cheng <thaumy.love@gmail.com>
2025-12-22 09:36:24 +0800

    upgrade nvim-nightly to 25-12-20

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
       inputs.nixpkgs.follows = "pkgs";
     };
 
-    nvim-nightly.url = "github:nix-community/neovim-nightly-overlay/ac9bde3d5fcec88383a16a63dc1bdb1702f7233e"; # 25-12-10
+    nvim-nightly.url = "github:nix-community/neovim-nightly-overlay/7e47a8c64312e726aafc0789a5d9043501a7e3ae"; # 25-12-20
     libnvimcfg.url = "path:./neovim/lib";
   };
 
