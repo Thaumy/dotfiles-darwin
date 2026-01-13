f78c8d7f0446281ae82d989e4cbf8963aaff806e
Thaumy Cheng <thaumy.love@gmail.com>
2026-01-13 16:44:50 +0800

    ignore patch files for git

--- hm/app/git.nix
+++ hm/app/git.nix
@@ -7,7 +7,13 @@ in
     enable = true;
 
     lfs.enable = true;
-    ignores = [ ".idea" ".vscode" ".thaumy" ".DS_Store" ];
+    ignores = [
+      ".idea"
+      ".vscode"
+      ".thaumy"
+      "*.patch"
+      ".DS_Store"
+    ];
     includes = [
       { path = "${homeDir}/cfg/git/config.ini"; }
       { path = "${homeDir}/cfg/git/aliases.ini"; }
