Thaumy/dotfiles

max to 3 qf lists

nvim/lua/opt.lua
@@ -41,6 +41,9 @@ vim.g.loaded_node_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_python3_provider = 0
 
+-- max qflist stack len
+vim.o.chi = 3
+
 function QFTF(opts)
   local list = nil
   if opts.quickfix == 1 then

Thaumy/dotfiles-darwin

nvim/lua/opt.lua
@@ -38,3 +38,6 @@ vim.g.loaded_perl_provider = 0
 vim.g.loaded_node_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_python3_provider = 0
+
+-- max qflist stack size
+vim.go.chi = 3