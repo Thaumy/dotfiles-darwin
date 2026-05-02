8fdaaf500657d0c445e4240216d6457a26a9a7b9
Thaumy Cheng <thaumy.love@gmail.com>
2026-05-02 23:37:25 +0800

    remove useless sccache env var sets
    Launchd is a shit. Setting global env var for GUI apps sucks.
    I'll migrate all my env var sets into alacritty's config.

--- os/app/sccache.nix
+++ os/app/sccache.nix
@@ -3,13 +3,8 @@ let
   cache_dir = "/var/cache/sccache";
 in
 {
-  environment = {
-    systemPackages = [ pkgs.sccache ];
-    variables = {
-      SCCACHE_DIR = cache_dir;
-      SCCACHE_CACHE_SIZE = "20G";
-    };
-  };
+  environment.systemPackages = [ pkgs.sccache ];
+
   system.activationScripts.postActivation.text = ''
     mkdir -p ${cache_dir}
     chmod 770 ${cache_dir}
