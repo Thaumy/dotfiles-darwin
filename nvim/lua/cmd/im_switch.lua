9c61a9fb8fd4f493a2ddaa3220088f4aea4f2267
Thaumy Cheng <thaumy.love@gmail.com>
2026-05-09 13:11:55 +0800

    reduce table indexing

--- nvim/lua/cmd/im_switch.lua
+++ nvim/lua/cmd/im_switch.lua
@@ -1,8 +1,10 @@
+local vim_api = vim.api
+
 if vim.env.USER == 'root' then return end
 
 local activate_next = false
 
-vim.api.nvim_create_autocmd('InsertEnter', {
+vim_api.nvim_create_autocmd('InsertEnter', {
   callback = function()
     if activate_next then
       vim.system { 'macism', 'com.apple.inputmethod.SCIM.Shuangpin' }
@@ -10,7 +12,7 @@ vim.api.nvim_create_autocmd('InsertEnter', {
   end,
 })
 
-vim.api.nvim_create_autocmd('InsertLeave', {
+vim_api.nvim_create_autocmd('InsertLeave', {
   callback = function()
     local obj = vim.system({ 'macism' }, { text = true }):wait()
     activate_next = obj.stdout == 'com.apple.inputmethod.SCIM.Shuangpin\n'
