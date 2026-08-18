ba541a150f90f388a80ceec59eb2d799645a361a
Thaumy Cheng <thaumy.love@gmail.com>
2026-08-18 17:52:52 +0800

    use fully-qualified brew name

--- os/app/homebrew.nix
+++ os/app/homebrew.nix
@@ -1,11 +1,8 @@
 _: {
   homebrew = {
     enable = true;
-    taps = [
-      "laishulu/homebrew"
-    ];
     brews = [
-      "macism"
+      "laishulu/homebrew/macism"
     ];
     casks = [
       "mos"
